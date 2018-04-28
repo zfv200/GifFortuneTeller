6ce061d8de6925674bd17cb7edac72da010dfc32
github username <github email address>
2018-04-28 15:44:43 -0400

    can now save gifs

---
+++ src/components/SavedFortune.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SavedFortune = (props) => {
+  return (
+    <div>
+      <h2>On {props.fortune.date}:</h2>
+      <h3>{props.fortune.message}</h3>
+      <img src={props.fortune.gif} alt="super cool gif"/>
+    </div>
+  )
+}
+
+export default SavedFortune
